d6ff1e253db8e5df7eccd9f38854e7bfc5735408
koocyton <koocyton@gmail.com>
2018-04-22 19:35:29 +0800

    开始处理长连接部分

--- src/main/java/com/doopp/gauss/common/listener/GameSocketListener.java
+++ src/main/java/com/doopp/gauss/common/listener/GameSocketListener.java
@@ -16,12 +16,6 @@ public class GameSocketListener extends WebSocketListener {
     private Map<Long, Channel> channelGroup = new HashMap<>();
 
     @Override
-    public void onConnect(Channel channel) {
-        Long userId = 123L;
-        channelGroup.put(userId, channel);
-    }
-
-    @Override
     public void onTextMessage(Channel channel, String message) {
         channel.writeAndFlush(new TextWebSocketFrame("[you say]" + message));
     }
@@ -29,6 +23,24 @@ public class GameSocketListener extends WebSocketListener {
     @Override
     public void onJsonMessage(Channel channel, Object json) {
         channel.writeAndFlush(new TextWebSocketFrame("[you say]" + (new Gson()).toJson(json)));
+    }
+
+    private Long getChannelUserId(Channel channel) {
+        return channel.getUserId();
+    }
+
+    private void sendMessage(Long userId, String message) {
+        Channel channel = channelGroup.get(userId);
+        if (channel!=null) {
+            channel.writeAndFlush(new TextWebSocketFrame(message));
+        }
+    }
+
+    @Override
+    public void onConnect(Channel channel) {
+        Long userId = 123L;
+        channelGroup.put(userId, channel);
+        channelGroup.get(userId).close();
     }
 
     @Override
